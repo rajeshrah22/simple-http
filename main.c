6120b928ddd8290cc147519346c10b0d8480a544
Rahul Rajesh <rajeshrah22@gmail.com>
2024-07-05 10:57:17 -0400

    in progress

--- main.c
+++ main.c
@@ -2,32 +2,35 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 
 #define BUFSIZE 1024 * 64
 #define MAX_HEADERS 64
 #define MAX_BODY_SIZE 1024
 
-struct http_request_line {
-  char method[16];
-  char version[16];
-  char uri[256];
-};
-
-struct http_header {
-  char value[256];
-  char name[64];
-};
-
 struct URI {
+  int uri_length;
   char path[256];
   char query_string[256];
 };
 
+struct http_request_line {
+  struct URI uri;
+  char method[16];
+  char version[16];
+};
+
+struct http_header {
+  char name[64];
+  char value[256];
+};
+
 struct http_request {
+  int header_count;
   struct http_request_line request_line;
   struct http_header headers[MAX_HEADERS];
-  int header_count;
   char body[MAX_BODY_SIZE];
 };
 
@@ -37,7 +40,7 @@ int  main() {
   socklen_t peer_addr_size;
   char buf[BUFSIZE];
 
-  int sfd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0); 
+  int sfd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0);
   if (sfd == -1) {
     perror("socket failiure\n");
     return -1;
@@ -64,16 +67,28 @@ int  main() {
   struct http_request request;
 
   // PARSE REQUEST:
+  // get method
   int i = 0;
-  for(; i < n; i++)
+  for(; i < n; i++) {
     if(buf[i] != ' ')
       continue;
     else
       break;
+  }
   
-  strncpy(request.request_line.method, buf, i + 1);
-  printf("METHOD: %s\n", request.request_line.method);
-  // get request type
+  strncpy(request.request_line.method, buf, i);
+
+  int uri_idx = 0;
+  for(++i; i < n; i++) {
+    if (buf[i] == '?' || buf[i] == ' ')
+      break;
+    request.request_line.uri.path[uri_idx] = buf[i];
+    uri_idx++;
+  }
+
+  request.request_line.uri.uri_length = uri_idx;
+
+  printf("URI: %s | LEN: %d\n", request.request_line.uri.path, request.request_line.uri.uri_length);
 
   // deconstruct URL into scheme, server, path, query string
   // read headers [key]: [value]
@@ -84,5 +99,6 @@ int  main() {
   // headers
   // body
 
+  return 0;
 }
 
